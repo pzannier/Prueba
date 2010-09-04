45d255456148a38a537faf7689b406c9f2f30a01
Pam <pam@pam-laptop.(none)>
2010-09-03 22:47:36 -0400

    primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ayni::Application.config.secret_token = '3d3c86cd144d043f4f7e350754a8402b8d027c4ac990e876b384529bcd99d62c3530ddb76bda2f2a18b70e4cc36f355b2a5cbbcc6c17b22a493d5930cedb5427'
